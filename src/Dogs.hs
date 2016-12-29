d2756c32fa337b49b881e5265149c18f39657835
Enrique <abhean13@hotmail.com>
2016-12-29 17:36:47 +0100

    Chapter 11 (WIP)

---
+++ src/Dogs.hs
@@ -0,0 +1,24 @@
+module Dogs where
+
+data PugType = PugData
+data HuskyType a = HuskyData
+data DogueDeBordeaux doge = DogueDeBordeaux doge
+
+myPug = PugData :: PugType
+
+myHusky :: HuskyType a
+myHusky = HuskyData
+
+myOtherHusky :: Num a => HuskyType a
+myOtherHusky = HuskyData
+
+myOtherOtherHusky :: HuskyType [[[Int]]]
+myOtherOtherHusky = HuskyData
+
+myDodge :: DogueDeBordeaux Int
+myDodge = DogueDeBordeaux 10
+
+data Doggies a =
+    Husky a
+  | Mastiff a
+  deriving (Eq, Show)
