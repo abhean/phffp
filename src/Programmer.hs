27aae0b1fa2ed996a85fd61f44d557100434377d
abhean <Abhean13@hotmail.com>
2017-01-05 11:58:48 +0100

    Chapter 11 (WIP)

---
+++ src/Programmer.hs
@@ -0,0 +1,37 @@
+module Programmer where
+
+data OperatingSystem =
+  GnuPlusLinux
+  | OpenBSDPlusNevermindJustBSDStill
+  | Mac
+  | Windows
+  deriving (Eq, Show)
+
+data ProgrammingLanguage =
+  Haskell
+  | Agda
+  | Idris
+  | PureScript
+  deriving (Eq, Show)
+
+data Programmer =
+  Programmer { os :: OperatingSystem
+              , lang :: ProgrammingLanguage }
+              deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux
+  , OpenBSDPlusNevermindJustBSDStill
+  , Mac
+  , Windows ]
+
+allLanguages :: [ProgrammingLanguage]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers = [Programmer os lang | os <- allOperatingSystems, lang <- allLanguages]
+
+allProgrammers' :: [Programmer]
+allProgrammers' = foldr addOsProgrammers [] allOperatingSystems
+  where addOsProgrammers os programmers = foldr ((:) . Programmer os) programmers allLanguages
