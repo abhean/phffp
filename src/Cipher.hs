bea30e7330d794a95e760248aeb572d73de93a4c
abhean <Abhean13@hotmail.com>
2017-01-07 20:09:17 +0100

    Chapter 11 (WIP)

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -1,16 +1,32 @@
 module Cipher where
 
 import Data.Char
+import Data.List
+
+letterIndex :: Char -> Int
+letterIndex = (+ (-(ord 'a'))) . ord . toLower
+
+offsettedChar :: Int -> Char -> Char
+offsettedChar offset c =
+      toOriginalCharCase . chr . (+ firstLetterOrd) . (`mod` numLetters) . (+ offset) . (+ (-firstLetterOrd)) . ord . toLower $ c
+      where toOriginalCharCase = if isUpper c then toUpper else id
+            firstLetterOrd = ord 'a'
+            numLetters = (ord 'z' - firstLetterOrd) + 1
 
 cipherCaesar :: String -> Int -> String
-cipherCaesar s offset = map (offsetChar offset) s
-  where
-      offsetChar :: Int -> Char -> Char
-      offsetChar offset c =
-            toOriginalCharCase . chr . (+ firstLetterOrd) . (`mod` numLetters) . (+ offset) . (+ (-firstLetterOrd)) . ord . toLower $ c
-            where toOriginalCharCase = if isUpper c then toUpper else id
-                  firstLetterOrd = ord 'a'
-                  numLetters = (ord 'z' - firstLetterOrd) + 1
+cipherCaesar s offset = map (offsettedChar offset) s
 
 unCipherCaesar :: String -> Int -> String
 unCipherCaesar s offset = cipherCaesar s (-offset)
+
+cipherVignere :: String -> String -> String
+cipherVignere s password = result where
+  (result, pass) = foldl' cipherCharWithNextPassChar ("", cycle password) s
+   where cipherCharWithNextPassChar (result, pass) ' ' = (result ++ " ", pass)
+         cipherCharWithNextPassChar (result, pass) c   = (result ++ [offsettedChar (letterIndex . head $ pass) c], tail pass)
+
+unCipherVignere :: String -> String -> String
+unCipherVignere s password = result where
+  (result, pass) = foldl' unCipherCharWithNextPassChar ("", cycle password) s
+   where unCipherCharWithNextPassChar (result, pass) ' ' = (result ++ " ", pass)
+         unCipherCharWithNextPassChar (result, pass) c = (result ++ [offsettedChar (-(letterIndex . head $ pass)) c], tail pass)
