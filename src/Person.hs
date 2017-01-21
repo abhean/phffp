89c07cde01d2d520c48c98605da065a31a41ea74
abhean <Abhean13@hotmail.com>
2017-01-21 13:47:24 +0100

    Chapter 13

--- src/Person.hs
+++ src/Person.hs
@@ -1,11 +1,33 @@
 module Person where
 
---data Person = Person Bool deriving Show
---
---printPerson :: Person -> IO()
---printPerson person = putStrLn (show person)
+type Name = String
+type Age = Integer
 
-data Person = Person
-  { name :: String
-  , age  :: Int }
-  deriving (Eq, Show)
+data Person = Person Name Age deriving Show
+
+data PersonInvalid = NameEmpty
+                   | AgeTooLow
+                   | PersonInvalidUnknown String
+                   deriving (Eq, Show)
+
+mkPerson :: Name
+         -> Age
+         -> Either PersonInvalid Person
+
+mkPerson name age
+  | name /= "" && age > 0 = Right $ Person name age
+  | name == "" = Left NameEmpty
+  | not (age > 0) = Left AgeTooLow
+  | otherwise = Left $ PersonInvalidUnknown $
+                        "Name was: " ++ show name ++
+                        " Age was: " ++ show age
+
+gimmePerson :: IO()
+gimmePerson = do
+  putStrLn "Introduce a name: "
+  nameInput <- getLine
+  putStrLn "Introduce an age: "
+  ageInput <- getLine
+  case mkPerson nameInput (read ageInput) of
+    Right person -> putStrLn $ "Yay! Successfully got a person: " ++ show person
+    Left personInvalid -> putStrLn $ "Error found: " ++ show personInvalid
