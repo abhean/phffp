b7ebde6464fd8cdfcfff87977d51562d8d9f2d6d
abhean <Abhean13@hotmail.com>
2017-01-25 19:48:17 +0100

    Chapter 14 (WIP)

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,11 @@
-import Sing
+import Test.Hspec
+import WordNumber (digitToWord, digits, wordNumber)
+
 
 main :: IO ()
-main = print sing
+main = hspec $ do
+  describe "digitToWord" $ do
+    it "returns zero for 0" $ do
+      digitToWord 0 `shouldBe` "zero"
+    it "returns one for 1" $ do
+      digitToWord 1 `shouldBe` "???"
